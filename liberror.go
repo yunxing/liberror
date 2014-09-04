2a0f68e4f8e1ec81dab3a421f2050e651bc58132
Yunxing Dai <yunxing@upthere.com>
2014-09-04 15:48:48 -0700

    Updated intro doc

--- liberror.go
+++ liberror.go
@@ -1,3 +1,50 @@
+// Package liberror is a lightweight package for providing errors with richer information in Golang.
+//
+// For a full guide visit http://github.com/yunxing/liberror
+//
+//  package main
+//
+//  import "github.com/yunxing/liberror"
+//  import "fmt"
+//
+//  var NetworkFail ErrorType
+//
+//  func simulateNetworkFail() liberror.Error {
+//      return New(fmt.Errorf("No network connection"), NetworkFail, nil)
+//  }
+//
+//  func main() {
+//      // Register a new error type
+//      NetworkFail := liberror.RegisterError("NetworkFail")
+//
+//      err := simulateNetworkFail()
+//      if err != nil {
+//          fmt.Println(err)
+//      }
+//
+//      err = New(fmt.Errorf("File is corrupted"),
+//                liberror.DataCorruption, "/disk/a/report")
+//      fmt.Println(err)
+//
+//      // You can also compare an error by:
+//      if err.ErrorType == liberror.DataCorruption {
+//          // Get more info from the error
+//          fmt.Println(err.info)
+//      }
+//  }
+//
+// Some sample output:
+//   ~/g/s/g/y/liberror (master|…) $ go test
+//   Error reason : Generic
+//   Error message: test error
+//   Stack:
+//       _/Users/yunxing/go/src/github.com/yunxing/liberror.TestError
+//           at /Users/yunxing/go/src/github.com/yunxing/liberror/error_test.go:6
+//       testing.tRunner
+//           at /usr/local/go/src/pkg/testing/testing.go:374
+//       runtime.goexit
+//           at /usr/local/go/src/pkg/runtime/proc.c:1394
+//
 package liberror
 
 import "runtime"
@@ -73,6 +120,7 @@ type Error struct {
 	Info      interface{}
 }
 
+// Wrap an error with more information
 func New(err error, errorType ErrorType, userInfo interface{}) Error {
 	stack := make([]uintptr, 1024)
 	n := runtime.Callers(2, stack)
